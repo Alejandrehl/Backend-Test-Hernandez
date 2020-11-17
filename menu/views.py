3bb4603f602ccee17d6f715d2d331339f8c8586f
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-11-17 12:22:02 -0300

    Guardando avances, falta pasar un test de opciones

--- menu/views.py
+++ menu/views.py
@@ -14,3 +14,22 @@ class OptionViewSet(viewsets.GenericViewSet,
     permission_classes = (IsAuthenticated,)
     queryset = Option.objects.all()
     serializer_class = serializers.OptionSerializer
+
+    def get_queryset(self):
+        """Return objects"""
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(menu__isnull=False)
+
+        return queryset.order_by('-description').distinct()
+
+    # def list(self, request):
+    #     """List options"""
+    #     print("LIST OPTIONS")
+    #     print(request.user.is_staff)
+    #     queryset = self.queryset
+
+    #     return queryset
